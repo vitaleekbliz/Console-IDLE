7d23a4fda3fd4d37de1397af7e31fd17ca402afd
ViTaLeek <vitaleek.bliz@gmail.com>
2026-04-14 21:05:01 +0300

    working on object funtionality

--- App/Source/Objects/Object.h
+++ App/Source/Objects/Object.h
@@ -1,11 +1,13 @@
 #pragma once
+#include "Logging/Logging.h"
 
 class ResourcesManager;
+class ObjectCreator;
 
 class Object
 {
 public:
-	Object(ResourcesManager* resourcesManager);
+	Object(ResourcesManager* resourcesManager, ObjectCreator* objectCreator);
 	virtual ~Object() = default;
 
 	virtual void render() {};
@@ -15,7 +17,20 @@ public:
 	void setRenderMode(bool state);
 
 protected:
+	template <typename T, typename... Args>
+		requires std::derived_from<T, Object>
+	T* createObject(Args&&... args)
+	{
+		if (!m_ObjectCreator)
+		{
+			APP_ERROR("%s doesn't have Object Creator", typeid(*this).name())
+			return nullptr;
+		}
+		m_ObjectCreator->create()
+	}
+
 private:
 	ResourcesManager* m_ResourceManager = nullptr;
+	ObjectCreator* m_ObjectCreator = nullptr;
 	bool m_isRenderable = false;
 };
